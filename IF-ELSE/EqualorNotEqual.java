6657e67a57c7be6a42ebec08eba6f0283c1431f8
Srinitha R V <162591175+srinitha267@users.noreply.github.com>
2025-08-01 20:08:51 +0530

    Create EqualorNotEqual.java

---
+++ IF-ELSE/EqualorNotEqual.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if(a==b)
+            System.out.println("EQUAL");
+        else
+            System.out.println("NOT EQUAL");
+    }
+}
