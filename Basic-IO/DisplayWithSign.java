1f6c7687ea5ac5e610086c13ed71934c2f8d386d
Srinitha R V <162591175+srinitha267@users.noreply.github.com>
2025-08-01 20:34:43 +0530

    Create DisplayWithSign.java

---
+++ Basic-IO/DisplayWithSign.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        
+        if(a>=0)
+            System.out.println("+"+a);
+        else
+            System.out.println(a);
+    }
+}
