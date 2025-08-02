c40f096adbce31d19bee34dd89f5e82dbc51b502
Srinitha R V <162591175+srinitha267@users.noreply.github.com>
2025-08-02 16:27:21 +0530

    Create PrintingAlphabetsForNTimes.java

---
+++ Looping/PrintingAlphabetsForNTimes.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int i;
+        
+        while(a-->0){
+            for( i = 97; i <= 122; i++){
+                System.out.print((char)i+" ");
+        }
+            System.out.println();
+        }
+    }
+}
