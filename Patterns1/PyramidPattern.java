90e7649f1d7db4c85af435c4993c3adc839a0e26
Srinitha R V <162591175+srinitha267@users.noreply.github.com>
2025-08-05 22:03:42 +0530

    Create PyramidPattern.java

---
+++ Patterns1/PyramidPattern.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n,row,space,star;
+        n = sc.nextInt();
+        
+        for(row = n; row >= 1; row--){
+            for(space = 1; space <=n-row; space++)
+                System.out.print(" ");
+            for(star = 1; star <= row*2-1; star++)
+                System.out.print("*");
+                System.out.println();
+        }
+        }
+}
