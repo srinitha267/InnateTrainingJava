43cea888087350908e622ca026889e573b81f687
Srinitha R V <162591175+srinitha267@users.noreply.github.com>
2025-08-05 22:06:50 +0530

    Create X-Pattern.java

---
+++ Patterns1/X-Pattern.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int row,col;
+        n = 2 * n -1;
+        for(row=1; row<=n; row++){
+            for(col=1; col<=n; col++){
+                if(row == col || col + row == n+1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
