9b8999fb2270b49ace41c34a886340e0bfd7f83c
Srinitha R V <162591175+srinitha267@users.noreply.github.com>
2025-08-06 18:45:38 +0530

    Create BowTiePattern.java

---
+++ Patterns2/BowTiePattern.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int row,space,star;
+        
+        for(row=n;row>=1;row--){
+            for(star=1;star<=n-row+1;star++)
+                System.out.print("*");
+            for(space=1;space<=2*(row-1);space++)
+                System.out.print(" ");
+            for(star=1;star<=n-row+1;star++)
+                System.out.print("*");
+            System.out.println();
+        }
+        
+        for(row=1;row<=n;row++){
+            for(star=1;star<=n-row+1;star++)
+                System.out.print("*");
+            for(space=1;space<=2*(row-1);space++)
+                System.out.print(" ");
+            for(star=1;star<=n-row+1;star++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
