3466eae2aa33f6c70b4f801372621fd6fb02c46c
Srinitha R V <162591175+srinitha267@users.noreply.github.com>
2025-08-14 20:12:31 +0530

    Create SumWithoutCurrentElement.java

---
+++ Arrays/SumWithoutCurrentElement.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int size=sc.nextInt();
+        int a[] = new int[size];
+        
+        int totalsum = 0;
+        for(int i=0;i<size;i++){
+            a[i]=sc.nextInt();
+            totalsum += a[i];
+        }
+        for(int i=0;i<size;i++){
+            System.out.print(totalsum-a[i]+" ");
+        }
+    }
+}
