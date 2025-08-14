03cd696ede730a610cd1d356fa752f2ef0610665
Srinitha R V <162591175+srinitha267@users.noreply.github.com>
2025-08-14 19:56:33 +0530

    Create Reverse.java

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int a[] = new int[size];
+        
+        for(int i=0;i<size;i++){
+            a[i]=sc.nextInt();
+        }
+        for(int i=size-1;i>=0;i--){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
