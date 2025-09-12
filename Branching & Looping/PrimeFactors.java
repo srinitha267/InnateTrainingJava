dd569f1ac9dc4ee0afd69a2bf85f6745c816c70e
Srinitha R V <162591175+srinitha267@users.noreply.github.com>
2025-09-12 14:09:35 +0530

    Create PrimeFactors.java

--- Branching & Looping/PrimeFactors.java
+++ Branching & Looping/PrimeFactors.java
@@ -28,34 +28,4 @@ public class Solution {
         System.out.println(i+"->"+count);
     }
 }
-        import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
-public class Solution {
-
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc = new Scanner(System.in);
-        int n,i=2,count=0;
-        n = sc.nextInt();
-        
-        while(n!=1){
-            if(n%i==0){
-                count++;
-                n=n/i;
-            }
-            else{
-                if(count>0)
-                    System.out.println(i+"->"+count);
-                i++;
-                count = 0;
-            }
-                
-        }
-        System.out.println(i+"->"+count);
-    }
-}
         
